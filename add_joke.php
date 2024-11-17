11aa2e5749cd70a227b9e16a179e18880e0a4b53
jluthi-gcu <113372169+jluthi-gcu@users.noreply.github.com>
2024-11-17 16:28:21 -0700

    Update add_joke.php

--- add_joke.php
+++ add_joke.php
@@ -15,7 +15,7 @@ $userid = $_SESSION['userid'];
 
 echo "<h2>Trying to add a new joke " . $new_joke_question . " and " . $new_joke_answer . "</h2>";
 
-$stmt = $mysqli->prepare("INSERT INTO Jokes_table (JokeID, Joke_question, Joke_answer, user_id) VALUES (null, ?, ?, ?)");
+$stmt = $mysqli->prepare("INSERT INTO jokes_table (JokeID, Joke_question, Joke_answer, user_id) VALUES (null, ?, ?, ?)");
 $stmt->bind_param("ssi", $new_joke_question, $new_joke_answer, $userid);
 
 $stmt->execute();
