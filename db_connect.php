fab466022294cbc38f5ad71735d2f66fefa3f172
jluthi-gcu <113372169+jluthi-gcu@users.noreply.github.com>
2024-11-17 15:51:23 -0700

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -8,7 +8,7 @@ $host = "d13xat1hwxt21t45.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 $port = "3306";
 $username = "fh6zwc9dfg8xa3kr";
 $user_pass = "caxjh0i4pqejtgg7";
-$database_in_use = "jokes_db";
+$database_in_use = "xejxvoiettamng4x";
 
 
 $mysqli = new mysqli($host, $username, $user_pass, $database_in_use);
